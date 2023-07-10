fca2a5a6bab1c5f52b6e5ce5280aa445991b80b2
moth1995 <marcos-flores@live.com.ar>
2023-07-10 14:43:45 -0300

    Agregar archivos de proyecto.

---
+++ ConfigForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PES5_WE9_LE_GDB_Manager
+{
+    public partial class ConfigForm : Form
+    {
+        public ConfigForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PES5_WE9_LE_GDB_Manager
+{
+    public static class Utils
+    {
+        public static string GetFilename()
+        {
+            string fileName = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+            }
+            Console.WriteLine(fileName);
+            return fileName;
+        }
+
+        public static string GetFolder()
+        {
+            // Crear una instancia del cuadro de diálogo para seleccionar carpeta
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            string selectedPath = "";
+            // Verificar si se seleccionó una carpeta
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta de la carpeta seleccionada
+                selectedPath = folderBrowserDialog.SelectedPath;
+            }
+            Console.WriteLine(selectedPath);
+            return selectedPath;
+        }
+    }
+}
